22e95818d0d5a8936ccdb4b9dd43f8f6f3a28fba
der33425 <der33425@gmail.com>
2025-10-08 15:54:45 +0800

    新增帳密輸入系統

--- static/javascript/studentPage.js
+++ static/javascript/studentPage.js
@@ -1,7 +1,12 @@
 const studentUserName = document.querySelector("input#student_user_name");
 const studentPassword = document.querySelector("input#student_password");
+const submitButton = document.querySelector("button#submit_btn");
 
-if(studentUserName === 1 || studentPassword === 2){
+submitButton.addEventListener("click",distinguishInfo);
+
+function distinguishInfo(){
+    
+if(studentUserName.value === "1" && studentPassword.value === "2"){
 
 alert("Login Successfully!");
 
@@ -11,5 +16,5 @@ else{
 
 alert("Wrong Password or username, Please enter again.");
 
-}
-
+};
+};
