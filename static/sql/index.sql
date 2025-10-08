c580cd8224796c2a28cfef3d25d60a7c359e79b9
der33425 <der33425@gmail.com>
2025-10-08 14:36:43 +0800

    新增SQL

---
+++ static/sql/index.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE student(
+
+id PRIMARY KEY,
+student_name text,
+student_grade
+
+
+);
+
+INSERT INTO student VALUES (1,'test_student_1',80);
+INSERT INTO student VALUES (2,'test_student_2',60);
+INSERT INTO student VALUES (3,'test_student_3',70);
+
+SELECT * FROM student ORDER BY student_grade DESC;
