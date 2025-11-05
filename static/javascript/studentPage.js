b05ae48b7ca3ce30f39830a8819b8ad55a093464
der33425 <der33425@gmail.com>
2025-11-05 14:14:18 +0800

    JS更新

--- static/javascript/studentPage.js
+++ static/javascript/studentPage.js
@@ -2,18 +2,31 @@ const queryGradeBtn = document.querySelector("button#query_grade_btn");
 const queryRankingBtn = document.querySelector("button#query_ranking_btn")
 
 queryGradeBtn.addEventListener("click",display);
+queryRankingBtn.addEventListener("click", display)
 
 const studentData = {
 
-dataExists: true,
+gradeDataExists: true,
+rankingDataExists: true
 
 };
 
 function display(){
+i = 0
+while(i < 1){
 
-if(studentData.dataExists === true){
-alert("Hello World!");
+if(studentData.gradeDataExists === true){
+alert("六十分!");
+i++;
+console.log(i);
 
+};
+if(studentData.rankingDataExists === true){
+
+alert("第八名!");
+i++;
+
+};
 }
 
 }
